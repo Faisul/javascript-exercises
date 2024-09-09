9d84d175d7cbc99ebbdf15eacf5fe929f48b3276
Faisul <reachfais@gmail.com>
2024-09-09 20:57:57 +0530

    add code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    let concatText = '';
+    if (count <0){
+        return "ERROR";
+    }
+    for(let i=0; i<count; i++) {
+        concatText += word;
+    }
+    return concatText;
 };
 
 // Do not edit below this line
