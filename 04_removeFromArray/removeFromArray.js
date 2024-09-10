edd4655adbdd55e7bda313967dfe382675b65451
Faisul <reachfais@gmail.com>
2024-09-10 06:54:34 +0530

    code added

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let newArray = new Array();
+    array.forEach(element => {
+        if(!elements.includes(element)) 
+          newArray.push(element);
+    });
+   
+    return newArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
