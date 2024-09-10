53c08fbeaac48f794599f66b60c751b9c0ad3e41
Faisul <reachfais@gmail.com>
2024-09-10 07:18:42 +0530

    adding working code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!isPositiveInteger(start) || !isPositiveInteger(end)){
+        return "ERROR";
+    }
+    let index;
+    let max;
+    if(start <= end) {
+        index = start;
+        max = end;
+    } else {
+        index = end;
+        max = start;
+    }
+     
+   
+    let total =0;
+    for (let i=index; i<=max; i++) {
+        total += i;
+    }
+    return total;
+};
 
+let isPositiveInteger = function(number) {
+   return ("number" === typeof number)
+   && number > 0
+   && ( number - Math.floor(number) == 0);     
 };
 
 // Do not edit below this line
