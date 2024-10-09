ec7ee50651e353c3045064332e5b9d93befae962
Faisul Ihshan <faisul.ihshan@8common.com>
2024-10-09 16:00:45 +1100

    added solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    console.log("string= "+ str);
+    if (str.length ==1) {
+        return true;
+    }
+    //remove special charaters and space
+    let cleanedStr = str.replaceAll(",", "").replaceAll(".", "").replaceAll("!", "").replaceAll("'", "").replaceAll(" ","").toLowerCase();
+   console.log("cleaned string= "+ cleanedStr);
+    let size = cleanedStr.length-1;
+   for ( let i=size; i >=size/2; i--) {
+       // console.log("comparing "+ cleanedStr.charAt(i) + " [vs] " + cleanedStr.charAt(size - i));
+        if(cleanedStr.charAt(i) != cleanedStr.charAt(size - i)) {
+            return false;
+        }
+   }
+   return true;
 
 };
 
