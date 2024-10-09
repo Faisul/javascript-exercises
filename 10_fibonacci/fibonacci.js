b048b43d8994584118a8603e827fc6d2b4e12dd7
Faisul Ihshan <faisul.ihshan@8common.com>
2024-10-09 16:29:30 +1100

    adding fibonacci soution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if( typeof num != 'number') {
+        num = parseInt(num);
+    }
+
+    if (num < 0) {
+        return "OOPS";
+    } else if ( num == 0 || num == 1) {
+        return num;
+    }
+
+    let prevNumber = 1;
+    let currentNumber = 0;
+    let counter =1;
+    while (counter <= num) {
+        temp = currentNumber;
+        currentNumber += prevNumber;
+        prevNumber = temp;
+        counter ++;
+    }
+    return currentNumber;
+   
 };
 
 // Do not edit below this line
