60580e0aabc60ed27c83adeb98a23b007f19de46
Faisul Ihshan <faisul.ihshan@8common.com>
2024-10-09 17:24:59 +1100

    adding the solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    // let oldest =0;
+    // peopleArr.reduce(( oldest, people) => {
+    //     let age = people["yearOfDeath"] - people["yearOfBirth"];
+    //     oldest = age > oldest? age: oldest;
+    //     console.log(people);
+    //     console.log(oldest);
+    //     return people;
+    //  }, 0);
 
+
+  return peopleArr.reduce(
+    (oldestPerson, people) =>  getAge(people) > getAge(oldestPerson) ? people : oldestPerson
+    , peopleArr[0]
+   );
 };
+
+function getAge(people) {
+    let yearOfDeath = 0;
+    if( people.hasOwnProperty("yearOfDeath")) {
+        yearOfDeath = people["yearOfDeath"];
+    } else {
+        yearOfDeath = new Date().getFullYear();
+    }
+
+
+    return  yearOfDeath - people["yearOfBirth"];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
